433ed449c4db5adcb7f9c0ac52852b422ec2035a
northkorean-go <johni98@naver.com>
2026-04-11 03:11:28 +0900

    Update logout.js
    Update

--- functions/api/logout.js
+++ functions/api/logout.js
@@ -1,1 +1,19 @@
+const { json, clearAdminCookie } = require("./_utils");
 
+module.exports = async (req, res) => {
+  try {
+    if (req.method !== "POST") {
+      return json(res, 405, { error: "Method Not Allowed" });
+    }
+
+    clearAdminCookie(res);
+
+    return json(res, 200, {
+      success: true,
+      message: "로그아웃 완료",
+    });
+  } catch (error) {
+    console.error("logout error:", error);
+    return json(res, 500, { error: "로그아웃 처리 중 오류" });
+  }
+};
