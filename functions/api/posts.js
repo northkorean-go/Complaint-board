b6a7dc0899a5a9f6eb7dde74d869948e64b5204c
northkorean-go <johni98@naver.com>
2026-04-11 03:51:44 +0900

    Update posts.js
    Update

--- functions/api/posts.js
+++ functions/api/posts.js
@@ -1,5 +1,8 @@
 export async function onRequest(context) {
-  return new Response(JSON.stringify([{ ok: true, route: "/api/posts" }]), {
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-  });
+  return new Response(
+    JSON.stringify({ ok: true, route: "/api/posts" }),
+    {
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
