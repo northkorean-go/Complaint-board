af52fe3962405b858eb2d2d9fe7fdd72c8fddd1f
northkorean-go <johni98@naver.com>
2026-04-10 23:36:31 +0900

    Update comment.js

--- functions/api/comment.js
+++ functions/api/comment.js
@@ -1,1 +1,24 @@
+export async function onRequestPost({ request, env }) {
+  const { postId, content } = await request.json();
 
+  if (!content) {
+    return new Response(JSON.stringify({ msg: "내용 입력" }), {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  const date = new Date().toLocaleString("ko-KR", {
+    timeZone: "Asia/Seoul",
+  });
+
+  await env.DB.prepare(
+    "INSERT INTO comments (post_id, content, date) VALUES (?, ?, ?)"
+  )
+    .bind(postId, content, date)
+    .run();
+
+  return new Response(JSON.stringify({ ok: true }), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
