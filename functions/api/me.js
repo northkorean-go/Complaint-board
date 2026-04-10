3186bdef02d9a58eb23f3f2fa8a2eabc4cdf089c
northkorean-go <johni98@naver.com>
2026-04-11 04:13:43 +0900

    Update me.js
    Update

--- functions/api/me.js
+++ functions/api/me.js
@@ -1,18 +1,7 @@
-const { json, isAdmin } = require("./_utils");
+import { json, isAdmin } from "./_utils.js";
 
-async function onRequest(context) {
-  try {
-    return json(
-      {
-        isAdmin: isAdmin(context.request),
-      },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("me error:", error);
-    return json({ error: "로그인 상태 확인 실패" }, { status: 500 });
-  }
+export async function onRequestGet(context) {
+  return json({
+    isAdmin: isAdmin(context.request),
+  });
 }
-
-module.exports = { onRequest };
-
