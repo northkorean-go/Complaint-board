ec5783c89e90a48af72baef433e94107928f32c9
northkorean-go <johni98@naver.com>
2026-04-11 03:10:55 +0900

    Update me.js
    Update

--- functions/api/me.js
+++ functions/api/me.js
@@ -1,1 +1,15 @@
+const { json, parseCookies } = require("./_utils");
 
+module.exports = async (req, res) => {
+  try {
+    const cookies = parseCookies(req);
+    const isAdmin = cookies.adminAuth === "ok";
+
+    return json(res, 200, {
+      isAdmin,
+    });
+  } catch (error) {
+    console.error("me error:", error);
+    return json(res, 500, { error: "로그인 상태 확인 실패" });
+  }
+};
