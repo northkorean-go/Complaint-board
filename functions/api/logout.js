0f2d460ac12e5008ea4cbfe6ee5f22c41bfdbba7
northkorean-go <johni98@naver.com>
2026-04-11 19:28:25 +0900

    Update logout.js

--- functions/api/logout.js
+++ functions/api/logout.js
@@ -1,10 +1,12 @@
 export async function onRequestPost() {
-  return new Response(JSON.stringify({ ok: true }), {
+  return new Response(JSON.stringify({ success: true }), {
     status: 200,
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
-      "Set-Cookie":
-        "admin_auth=; Path=/; HttpOnly; SameSite=Lax; Secure; Max-Age=0",
-    },
+      "Set-Cookie": [
+        "admin_auth=; Path=/; HttpOnly; Max-Age=0; SameSite=Lax",
+        "admin_auth=; Path=/; HttpOnly; Expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Lax"
+      ].join(", ")
+    }
   });
 }
