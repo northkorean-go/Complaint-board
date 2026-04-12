0bc952499b551c23bd933509d5e997d2e3f97354
northkorean-go <johni98@naver.com>
2026-04-12 22:39:32 +0900

    Update match-detail.js

--- functions/api/match-detail.js
+++ functions/api/match-detail.js
@@ -1,1 +1,19 @@
+export async function onRequest({ request, env }) {
+  const id = new URL(request.url).searchParams.get('id');
 
+  const item = await env.DB.prepare(`
+    SELECT * FROM match_results WHERE id = ?
+  `).bind(id).first();
+
+  const rows = await env.DB.prepare(`
+    SELECT * FROM match_rows WHERE result_id = ? ORDER BY row_no
+  `).bind(id).all();
+
+  return new Response(JSON.stringify({
+    ok: true,
+    item: {
+      ...item,
+      rows: rows.results
+    }
+  }), { headers: { 'Content-Type': 'application/json' } });
+}
